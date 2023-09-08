b74a795377559bdb59e3acdc45b461ccf6c591db
Lee Hoon Lim <hoonlim123@gmail.com>
2023-09-08 17:31:07 +0800

    almost done navbar

--- Src/script.js
+++ Src/script.js
@@ -1,4 +1,4 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.getElementById("login-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Get input values
@@ -9,3 +9,12 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     // For this example, we'll just display an alert with the values
     alert("Username: " + username + "\nPassword: " + password);
 });
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const rightSec = document.getElementsByClassName('right-section')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+    rightSec.classList.toggle('active')
+})
