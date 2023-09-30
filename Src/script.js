9799bc5b3f0a83cbba9d10ffc6dc9d776bde67ce
Lee Hoon Lim <hoonlim123@gmail.com>
2023-09-30 11:07:31 +0800

    remember me box

--- Src/script.js
+++ Src/script.js
@@ -6,7 +6,6 @@ document.getElementById("login-form").addEventListener("submit", function (event
     var password = document.getElementById("password").value;
 
     // You can add your authentication logic here
-    // For this example, we'll just display an alert with the values
     alert("E-mel: " + username + "\nKatalaluan: " + password);
 });
 
