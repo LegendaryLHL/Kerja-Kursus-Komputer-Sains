0729ddc848efdc4b6b34f227d0fa0aeec822d061
Lee Hoon Lim <hoonlim123@gmail.com>
2023-10-15 17:59:46 +0800

    gps impliment

--- docs/IsiKehadiran.js
+++ docs/IsiKehadiran.js
@@ -1,31 +1,69 @@
-async function getGPSLocation() {
-    var gpsLocation = "Not Set";
-    if ("geolocation" in navigator) {
-        try {
-            const position = await navigator.geolocation.getCurrentPosition();
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            gpsLocation = "Latitude: " + latitude + ", Longitude: " + longitude;
-        } catch (error) {
-            gpsLocation = "Unable to retrieve GPS location: " + error.message;
+async function reverseGeocode(latitude, longitude) {
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`);
+        const data = await response.json();
+
+        if (data && data.display_name != undefined || data.display_name != null) {
+            const locationName = data.display_name;
+            return locationName;
+        } else {
+            return "Location not found";
         }
-    } else {
-        gpsLocation = "Geolocation is not supported in your browser.";
+    } catch (error) {
+        return "Error occurred during reverse geocoding" + error;
     }
-    return gpsLocation;
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
+
+// Display the updated GPS location
+function displayGPSLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    reverseGeocode(latitude, longitude)
+        .then(locationName => {
+            document.getElementById("gps-location").textContent = locationName;
+        })
+        .catch(error => {
+            handleLocationError(error);
+        });
+}
+
+function handleLocationError(error) {
+    var errorMessage = "Unable to retrieve GPS location: " + error.message;
+    document.getElementById("gps-location").textContent = errorMessage;
+}
+
+// Watch for changes in GPS location
+var watchID;
+async function watchGPSLocation() {
+    if ("geolocation" in navigator) {
+        try {
+            watchID = navigator.geolocation.watchPosition(displayGPSLocation, handleLocationError);
+        } catch (error) {
+            handleLocationError(error);
+        }
+    } else {
+        handleLocationError(new Error("Geolocation is not supported in your browser."));
+    }
+}
+
+// To stop watching GPS location (if needed)
+function stopWatchingGPSLocation() {
+    if (watchID) {
+        navigator.geolocation.clearWatch(watchID);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     // Get the current date and display it
     var currentDate = new Date();
     document.getElementById("date").textContent = currentDate.toDateString();
 
-    // Get and display the GPS location asynchronously
-    var gpsLocation = await getGPSLocation();
-    document.getElementById("gps-location").textContent = gpsLocation;
+    // Start watching the GPS location
+    watchGPSLocation();
 
     // Handle form submission
-    document.getElementById("input-form").addEventListener("submit", function (event) {
+    document.getElementById("bullet-choice").addEventListener("submit", function (event) {
         event.preventDefault();
         // Get the selected value from the "Can Go to Work" selector
         var canGoToWork = document.querySelector('input[name="can-go-work"]:checked').value;
