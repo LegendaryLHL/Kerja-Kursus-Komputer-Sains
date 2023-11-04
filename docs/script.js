20aae4451be74c2aec5a0d053d2b47398a65ce41
Lee Hoon Lim <hoonlim123@gmail.com>
2023-11-04 19:26:18 +0800

    expand

--- docs/script.js
+++ docs/script.js
@@ -2,11 +2,19 @@ document.getElementById("login-form").addEventListener("submit", function (event
     event.preventDefault();
 
     // Get input values
-    var username = document.getElementById("email").value;
+    var username = document.getElementById("ic-number").value;
     var password = document.getElementById("password").value;
 
     // Authentication logic here
 
     // Redirect to the new page after form submission
     window.location.href = "IsiKehadiran.html";
+});
+document.getElementById("infoExpandButton").addEventListener("click", function () {
+    var content = document.getElementById("infoExpandContent");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
 });
