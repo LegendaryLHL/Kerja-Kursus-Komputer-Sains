da65dc989df461992b83e737e4e0b623cba1fa0a
Lee Hoon Lim <hoonlim123@gmail.com>
2023-12-18 11:58:58 +0800

    konfigurasi pekerja

---
+++ docs/KonfigurasiPekerja.js
@@ -0,0 +1,44 @@
+function searchGrid() {
+    let input, filter, grid, card, name, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    grid = document.getElementsByClassName("worker-grid")[0];
+    card = grid.getElementsByClassName("worker-card");
+
+    for (i = 0; i < card.length; i++) {
+        name = card[i].getElementsByClassName("worker-name")[0];
+        if (name) {
+            txtValue = name.textContent || name.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                card[i].style.display = "";
+            } else {
+                card[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function handleRemoveButtonClick(event) {
+    const card = event.currentTarget.closest('.worker-card');
+
+    const name = card.querySelector('.worker-name').innerText;
+
+    alert('Clicked row content: Name - ' + name);
+}
+
+const removeButtons = document.getElementsByClassName('remove-button');
+for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', handleRemoveButtonClick);
+    removeButtons[i].style.cursor = 'pointer';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.querySelector('.add-worker-button');
+
+    addButton.addEventListener('click', handleAddButtonClick);
+
+    function handleAddButtonClick(event) {
+        alert('Add worker button clicked!');
+    }
+});
+
