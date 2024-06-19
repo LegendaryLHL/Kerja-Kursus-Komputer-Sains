e21c1134443fd54257140866fa4aed854420e568
Lee Hoon Lim <hoonlim123@gmail.com>
2024-06-19 20:53:47 +0800

    add offline libaries

--- docs/AnalisisKehadiran.php
+++ docs/AnalisisKehadiran.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="lib/all.min.css" />
   <link rel="stylesheet" type="text/css" href="css/AnalisisKehadiran.css" />
   <link rel="stylesheet" type="text/css" href="css/common.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+  <link rel="stylesheet" href="lib/flatpickr.min.css">
+  <script src="lib/flatpickr.js"></script>
   <title>PkjKehadiran</title>
 </head>
 
